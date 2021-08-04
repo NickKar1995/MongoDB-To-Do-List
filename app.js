0574bd94f8cc0927d5ce5c8e4d44b9ca21dbc38b
NickTriesReallyHard <79810433+Xontrokolis@users.noreply.github.com>
2021-08-04 19:30:10 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,102 +1,85 @@
-const express = require('express')
+const express = require("express");
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
-const app=express();
-const mongoose=require('mongoose')
+const app = express();
+const mongoose = require("mongoose");
 
-app.use(express.urlencoded( {extended:true} ))
+app.use(express.urlencoded({ extended: true }));
 
-app.set('view engine','ejs')
-app.use(express.static('public'))
-
-let items=['Buy Food','Cook Food', 'Eat Food']
-
-// const error=require(__dirname + '/error.js')
+app.set("view engine", "ejs");
+app.use(express.static("public"));
 
 // Here we connect Mongoose!
 
-mongoose.connect('mongodb://localhost:27017/todolistDB',{useNewUrlParser:true})
+mongoose.connect(
+  "mongodb://localhost:27017/todolistDB",
+  { useNewUrlParser: true },
+  { useUnifiedTopology: true }
+);
 
-const itemsSchema =  ({
-    name:String,
-})
+const itemsSchema = {
+  name: String,
+};
 
-const Item = mongoose.model('Item',itemsSchema)
+const Item = mongoose.model("Item", itemsSchema);
 
+const item1 = new Item({
+  name: "Welcome",
+});
 
+const item2 = new Item({
+  name: "Hit the + button to add a new item.",
+});
 
-// remove everything
-    
-// Item.remove({},(error)=>{
-//     if (error){
-//                 console.log('Fuck')
-//             }else{
-//                 console.log('YES')
-//             }
-// })
+const item3 = new Item({
+  name: "<-- Hit this to delete an item.",
+});
 
-//here end remove everything
+const defaultItems = [item1, item2, item3];
 
-app.get('/',(req,res)=>{
+app.get("/", function (req, res) {
+  Item.find({}, function (err, foundItems) {
+    if (foundItems.length === 0) {
+      Item.insertMany(defaultItems, function (err) {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log("Successfully savevd default items to DB.");
+        }
+      });
+      res.redirect("/");
+    } else {
+      res.render("list", { listTitle: "Today", newListItems: foundItems });
+    }
+  });
+});
 
-
-
-//here if you fuck up
-Item.find({},(error,foundItems)=>{
-    if(foundItems.length === 0){
-        ItemModel.insertMany(defaultItems, (err) => { (err) ? console.log(err) : console.log("Succesfully saved defaults items to DB.") });
-        foundItems = defaultItems;
-      }
-   
-      res.render("list", {listTitle: "Today", newListItems: foundItems});
-   
-    });
-})
-
-// app.get('/:customListName',(req,res)=>{
-//    const customListName = req.params.customListName
-
-// })    
-    
-
-
-app.post('/',(req,res)=>{
-    const itemName=req.body.newItem
-    const item= new Item({
-        name:itemName
-    })
-    item.save()
-    res.redirect('/')
-    // item=req.body.newItem
-    // items.push(item)
-    // res.redirect('/')
-    // console.log(item.length)
-})
-
+app.post("/", (req, res) => {
+  const itemName = req.body.newItem;
+  const item = new Item({
+    name: itemName,
+  });
+  item.save();
+  res.redirect("/");
+});
 
 //Delete Route
 
-
-app.post('/delete',(req,res)=>{
-   const  checkedItemId= req.body.checkbox
-   Item.findByIdAndRemove(checkedItemId,(error)=>{
-    if (error){
-                console.log('Problem')
-            }else{
-                console.log('Success')
-                res.redirect('/')
-            }
-
-   })
-})
-
-
-
+app.post("/delete", (req, res) => {
+  const checkedItemId = req.body.checkbox;
+  Item.findByIdAndRemove(checkedItemId, (error) => {
+    if (error) {
+      console.log("Problem");
+    } else {
+      console.log("Success");
+      res.redirect("/");
+    }
+  });
+});
 
 //Listening Here
 
-
-app.listen(port,()=>{
-    console.log('Server up')
-})
+app.listen(port, () => {
+  console.log("Server up");
+});
